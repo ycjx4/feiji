99290614596a36e2978116e0a11991b559ea0dfe
Wu Feihui <shizhan563@gmail.com>
2026-01-06 11:25:05 +0800

    Update 02/7.2.6.py with comments for better clarity

--- 02/7.2.6.py
+++ 02/7.2.6.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-class Dog:
-    def __init__(self, name):
-        self.name = name
+class Dog:  # \/- 定义一个类
+    def __init__(self, name):  # 初始化方法，包含名称参数
+        self.name = name  # 设置实例的名称属性
     
-    def play(self):
-        print("汪汪汪！ 我是", self.name)
+    def play(self):  # 定义实例方法
+        print("汪汪汪！ 我是", self.name)  # 打印对象的名称属性
 
-
-dog = Dog("旺财")
-dog.play()
+dog = Dog("旺财")  # 创建对象，并初始化实例属性
+dog.play()  # 调用类的实例方法
