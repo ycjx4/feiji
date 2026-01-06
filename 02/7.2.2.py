fdb2470afabeea3dae1aa627c23bb78bd3b0fe60
Wu Feihui <shizhan563@gmail.com>
2026-01-06 11:23:32 +0800

    Add comment annotations to code in the 02 directory for OOP clarity

--- 02/7.2.2.py
+++ 02/7.2.2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-class EmptyClass:
+class EmptyClass:  # 定义一个空类
     pass
 
-
-empty = EmptyClass()
-print(type(empty))
+empty = EmptyClass()  # 创建类实例对象（演示类实例化过程）
+print(type(empty))  # 打印实例对象的类型
