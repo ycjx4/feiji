432544a80a408c92a308ce4e07541906e2657314
Wu Feihui <shizhan563@gmail.com>
2026-01-06 14:35:45 +0800

    添加注释: 7.2.9.py - 私有方法示例

--- 02/7.2.9.py
+++ 02/7.2.9.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# 演示Python的私有方法
 class Dog:
+    # 双下划线开头的方法是私有方法，只能在类内部调用
     def __say(self, name):
         print(name)
 
+    # 公有方法：可以在类内部调用私有方法
     def play(self):
-        self.__say("汪汪汪")
+        self.__say("汪汪汪")  # 类内部可以调用私有方法
 
 
 dog = Dog()
-dog.play()
+dog.play()  # 正确：通过公有方法间接调用私有方法
 
-# 错误
+# 错误：私有方法无法从类外部直接调用
 dog.__say()
