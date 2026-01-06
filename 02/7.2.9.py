33844d6ebce96169484a9c8312917d20fe862315
Wu Feihui <shizhan563@gmail.com>
2026-01-06 11:25:21 +0800

    Update 02/7.2.9.py with modifications to Dog class comments

--- 02/7.2.9.py
+++ 02/7.2.9.py
@@ -2,15 +2,14 @@
 # -*- coding: utf-8 -*-
 
 class Dog:
-    def __say(self, name):
-        print(name)
+    def __say(self, name):  # 私有方法：只能类内部调用
+        print(name)  # 打印名称
+    
+    def play(self):  # 实例方法通过私有方法打印内容
+        self.__say("汪汪汪")  # 正确调用私有方法
 
-    def play(self):
-        self.__say("汪汪汪")
-
-
-dog = Dog()
-dog.play()
+dog = Dog()  # 创建对象
+dog.play()  # 调用方法，间接调用私有方法
 
 # 错误
-dog.__say()
+dog.__say()  # 直接调用私有方法会报错
