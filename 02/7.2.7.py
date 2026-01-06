522cff04c9d11d569c7151a38999ab2728ff2b85
Wu Feihui <shizhan563@gmail.com>
2026-01-06 14:35:29 +0800

    添加注释: 7.2.7.py - 私有属性示例

--- 02/7.2.7.py
+++ 02/7.2.7.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# 演示Python的私有属性（名称改写机制）
 class Dog:
     def __init__(self, name):
-        self.__name = name
+        self.__name = name  # 双下划线开头表示私有属性，外部无法直接访问
     
     def play(self):
-        print("汪汪汪！ 我是", self.__name)
+        print("汪汪汪！ 我是", self.__name)  # 类内部可以访问私有属性
 
 
 dog = Dog("旺财")
-dog.play()
+dog.play()  # 正确：通过方法间接访问私有属性
 
-# 错误
+# 错误：私有属性无法从类外部直接访问
 print(dog.__name)
